93dd76649c4a7d46ccc1cb58bb4f0f2da6d0cdbe
iAmMuadh <adeyemimuaz1@gmail.com>
2023-10-04 23:50:23 +0100

    added articles component

--- src/components/articles/Articles.tsx
+++ src/components/articles/Articles.tsx
@@ -1,5 +1,85 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
+// import required modules
+import { EffectCoverflow, Pagination } from "swiper/modules";
+
 const Articles = () => {
-  return <section id='articles'>Articles</section>;
+  const swiperSlide = "bg-center bg-cover w-[300px] h-[300px]";
+  const swiperSlideImage = "block w-full";
+  return (
+    <section id="articles" className="mb-16">
+      <h5>I write also</h5>
+      <h2>Articles</h2>
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="w-full py-[50px]"
+      >
+        <SwiperSlide className={swiperSlide}>
+          <img
+            src="https://swiperjs.com/demos/images/nature-1.jpg"
+            className={swiperSlideImage}
+          />
+        </SwiperSlide>
+        <SwiperSlide className={swiperSlide}>
+          <img
+            src="https://swiperjs.com/demos/images/nature-2.jpg"
+            className={swiperSlideImage}
+          />
+        </SwiperSlide>
+        <SwiperSlide className={swiperSlide}>
+          <img
+            src="https://swiperjs.com/demos/images/nature-3.jpg"
+            className={swiperSlideImage}
+          />
+        </SwiperSlide>
+        <SwiperSlide className={swiperSlide}>
+          <img
+            src="https://swiperjs.com/demos/images/nature-4.jpg"
+            className={swiperSlideImage}
+          />
+        </SwiperSlide>
+        <SwiperSlide className={swiperSlide}>
+          <img
+            src="https://swiperjs.com/demos/images/nature-5.jpg"
+            className={swiperSlideImage}
+          />
+        </SwiperSlide>
+        <SwiperSlide className={swiperSlide}>
+          <img
+            src="https://swiperjs.com/demos/images/nature-6.jpg"
+            className={swiperSlideImage}
+          />
+        </SwiperSlide>
+        {/* <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+        </SwiperSlide> */}
+      </Swiper>
+    </section>
+  );
 };
 
 export default Articles;
