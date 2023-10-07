79457af9dc39c119bd0dcfe82944097a9f879c55
iAmMuadh <adeyemimuaz1@gmail.com>
2023-10-07 15:54:04 +0100

    disabled the download CV button

--- src/components/header/CTAs.tsx
+++ src/components/header/CTAs.tsx
@@ -1,9 +1,17 @@
 import CV from "../../assets/cv.pdf";
 
 const CTAs = () => {
+   const isDisabled = true;
   return (
     <div className=" mt-10 flex gap-4 justify-center items-center ">
-      <a href={CV} download className=" btn bg-[#d6d3d1]" disabled>
+      <a
+        href={isDisabled ? undefined : CV}
+        download
+        // className=" btn bg-[#d6d3d1]"
+        className={`btn bg-[#d6d3d1] ${
+          isDisabled ? "opacity-50 cursor-not-allowed" : ""
+        }`}
+      >
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary ">
